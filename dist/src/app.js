af3b9d6c9fb0f74b635826b2608c853dae9492db
Svitlana <ponomarchuk.svitlana.dev@gmail.com>
2024-04-15 09:25:13 +0300

    updated gitignore and helmet rulse

--- dist/src/app.js
+++ dist/src/app.js
@@ -9,18 +9,24 @@ import helmet from 'helmet';
 import indexRouter from './routes/routes.js';
 const app = express();
 app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+    useDefaults: true,
+    directives: {
+        'script-src': [`'self'`, `'unsafe-eval'`, `http://api.themoviedb.org/3`],
+        'img-src': [`'self'`, 'https: data:']
+    }
+}));
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public'));
+app.use(logger('dev'));
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 const PORT = process.env.PORT || 3001;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use('/', indexRouter);
 app.use(function (req, res, next) {
     next(createError(404));
